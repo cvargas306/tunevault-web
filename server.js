a3041c0a3eaee13fab6c49f3eacc334d50a694e3
cvargas306 <cavbcr@gmail.com>
2026-09-24 21:47:01 -0600

    Add basic css styling

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// Load the songs data from the JSON file
 const songs = require("./data/songs.json");
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+app.use(express.static("public"));
+
+// Middleware to parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded({ extended: true }));
 
+// Route to display the list of songs on the home page
 app.get("/", (req, res) => {
   res.render("home", { songs: songs });
 });
 
+// Route to display the details of a specific song by ID
 app.get("/song/:id", (req, res) => {
   const songId = parseInt(req.params.id, 10);
 
@@ -28,15 +34,18 @@ app.get("/song/:id", (req, res) => {
   res.render("detail", { song: song });
 });
 
+// Route to display the form for adding a new song
 app.get("/add", (req, res) => {
   res.render("add");
 });
 
+// Route to handle the submission of the new song form
 app.post("/add", (req, res) => {
   const maxId = songs.reduce(function (max, s) {
     return s.id > max ? s.id : max;
   }, 0);
 
+  // Create a new song object with the submitted data
   const newSong = {
     id: maxId + 1,
     title: req.body.title,
@@ -47,7 +56,8 @@ app.post("/add", (req, res) => {
   };
 
   songs.push(newSong);
-
+  
+  // Redirect to the home page after adding the new song
   res.redirect("/");
 });
 
