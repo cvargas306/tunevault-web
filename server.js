5fd248ffe23149259c105a5855fc9ef7b0239bf7
cvargas306 <cavbcr@gmail.com>
2026-09-24 21:21:46 -0600

    Add Song Detail and Add Song pages with in-memory data

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
-// Minimal server just to confirm the environment is set up correctly.
-// We'll expand this with real routes (Home, Song Detail, Add Song) next.
-
 const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const songs = require("./data/songs.json");
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.send("TuneVault Web is running!");
+  res.render("home", { songs: songs });
+});
+
+app.get("/song/:id", (req, res) => {
+  const songId = parseInt(req.params.id, 10);
+
+  const song = songs.find(function (s) {
+    return s.id === songId;
+  });
+
+  if (!song) {
+    res.status(404).send("Song not found");
+    return;
+  }
+
+  res.render("detail", { song: song });
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+app.post("/add", (req, res) => {
+  const maxId = songs.reduce(function (max, s) {
+    return s.id > max ? s.id : max;
+  }, 0);
+
+  const newSong = {
+    id: maxId + 1,
+    title: req.body.title,
+    artist: req.body.artist,
+    album: "Single",
+    duration: req.body.duration,
+    genre: req.body.genre,
+  };
+
+  songs.push(newSong);
+
+  res.redirect("/");
 });
 
 app.listen(PORT, () => {
